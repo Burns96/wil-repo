953a71401b4d595e430488a80567b45dc6c9e89c
Tyler Brown <tcbrown050@gmail.com>
2023-11-06 19:42:17 -0800

    finished setting up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './pages/Home'
+import Faq from './pages/Faq'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './App.css'
+import { Route, Routes } from 'react-router-dom' 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/faq' element={<Faq />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contactus' element={<ContactUs />} />
+        </Routes>
+        <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
