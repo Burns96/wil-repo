f63ce82d4ce01a8792ebe56ca594cfd94ca3cf6e
potentdojo <potentdojo@gmail.com>
2023-12-03 22:32:46 -0800

    fixed nav translate

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,9 @@
-// Header.js
 import React, { useState } from 'react';
-import Navigation from './Navigation'; // Import the Navigation component
+import Navigation from './Navigation'; // Adjust the import path as necessary
 import './Header.css';
 
-const Header = ({ setPage, setLanguage }) => {
+const Header = ({ setPage, setLanguage, texts }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const handleItemClick = (pageName) => {
-    setPage(pageName);
-    setIsMobileMenuOpen(false); // Close the mobile menu on item click
-  };
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen); // Toggle the mobile menu
@@ -24,27 +18,21 @@ const Header = ({ setPage, setLanguage }) => {
     <header className="header-container">
       <div className="top-bar">
         <img src="city_logo.png" alt="City Logo" />
-
-        {/* Language Selector Dropdown */}
         <select className="language-selector" onChange={(e) => handleLanguageChange(e.target.value)}>
           <option value="en">English</option>
           <option value="es">Español</option>
           <option value="fr">French</option>
           {/* Add more languages as needed */}
         </select>
-
-        {/* Toggle button for mobile view */}
         <button className="nav-toggle" onClick={toggleMobileMenu}>
           {isMobileMenuOpen ? 'Close' : 'Menu'}
         </button>
       </div>
-      {/* Search container accessible on mobile */}
       <div className="search-container">
         <input type="text" placeholder="Search..." />
         <button>Search</button>
       </div>
-      {/* Render the Navigation component */}
-      <Navigation setPage={setPage} />
+      <Navigation setPage={setPage} texts={texts} />
     </header>
   );
 };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,18 @@
-// Navigation.js
 import React from 'react';
+import './Navigation.css';
 
-const Navigation = ({ setPage }) => {
+// Updated to accept `texts` prop
+const Navigation = ({ setPage, texts }) => {
   const handleItemClick = (pageName) => {
     setPage(pageName);
   };
 
   return (
     <div className="navigation">
-      <a href="#home" onClick={() => handleItemClick('Home')}>Home</a>
-      <a href="#faq" onClick={() => handleItemClick('Faq')}>FAQ</a>
-      <a href="#contact" onClick={() => handleItemClick('ContactUs')}>Contact Us</a>
-      {/* Additional links can be added here */}
+      <a href="#home" onClick={() => handleItemClick('Home')}>{texts.home}</a>
+      <a href="#faq" onClick={() => handleItemClick('Faq')}>{texts.faq}</a>
+      <a href="#contact" onClick={() => handleItemClick('ContactUs')}>{texts.contactUs}</a>
+      {/* Additional links can be added here, using texts from the `texts` prop */}
     </div>
   );
 };
